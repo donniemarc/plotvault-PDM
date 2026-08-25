789f97e14f4692d7ccbb3f50c976dc1b41eba54a
donald_1010 <donald_1010@qq.com>
2026-08-25 15:47:34 +0800

    fix: 更新 CURRENT_VERSION 到 1.9.0

--- client/src/updater.ts
+++ client/src/updater.ts
@@ -7,7 +7,7 @@ export interface UpdateInfo {
 
 const GITHUB_REPO = 'donniemarc/plotvault-PDM';
 const GITHUB_API = `https://api.github.com/repos/${GITHUB_REPO}/releases/latest`;
-const CURRENT_VERSION = '1.8.0';
+const CURRENT_VERSION = '1.9.0';
 
 export async function checkForUpdates(): Promise<UpdateInfo | null> {
   try {
